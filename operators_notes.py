0e46fd09cb3f5bb8b72ad728c8f998f3d73d7f0f
Kamal Lal <kamalzinbox@gmail.com>
2023-01-03 01:28:30 +0400

    operator notes - initial commit

---
+++ operators_notes.py
@@ -0,0 +1,48 @@
+# Arithmatic Operators
+#     +  addition
+#     -  subtraction
+#     *  multiplication
+#     /  division (returns float)
+#     // floor division (returns just quotient)
+#     ** exponentiation (x**3 = x cubed)
+#     %  modulo (returns reminder)
+
+
+# Assignment Operator
+#     =  (a = 4)
+#     += (a += 4, meaning a = a + 4)
+#     -+, *=, /=, //=, %=, **=, &=, |=, ^=, >>=, <<=
+
+
+# Comparison Operator
+#     ==  Equal to
+#     !=  Not equal to
+#     <   Less than
+#     >   Greater than
+#     <=  Less than or equal to
+#     >=  Greater than or equal to
+
+
+# Logical Operator
+#     and
+#     or
+#     not
+
+
+# Bitwise Operator
+#     &  bitwise AND
+#     |  bitwise OR
+#     ^  bitwise XOR
+#     ~  bitwise NOT
+#     << Zero-fill left shift
+#     >> Zero-fill right shift
+
+
+# Membership Operator
+#     in
+#     not in
+
+
+# Identity Operator
+#     is
+#     is not
